264ea136b4a09af6e58a232b6d914a83574d3d34
shj0000 <shj0000@ex-it.co.kr>
2023-07-26 02:44:39 +0900

    작동 미확인 > 임시 저장

--- src/runner/copy/component/ComponentManager.tsx
+++ src/runner/copy/component/ComponentManager.tsx
@@ -36,7 +36,16 @@ function entityRenderer(
     // isAppend option
     // repository 부터 recoil
     // https://rjsf-team.github.io/react-jsonschema-form/
-    
+
+    // TODO :: render 연결 방법.. 문제는 여러 개 복합 칼럼 일 때. > 같은 name인 것들을 전부 해당 renderer로 전송?
+    // class 정의 시. @outer("name")
+    // class properties > @out("name")
+    // class 정의 시. @inner("name")
+    // class properties > @in("name")
+
+    // 문제는 반환된 state-repository mapping. react hook form?
+    // inner component에 update callback 세팅?
+
     return (
         <div>{entity}</div>
     )
