438d848ce097e84eec290028eb36fc06861e069e
shj0000 <shj0000@ex-it.co.kr>
2023-08-08 02:13:43 +0900

    작동 확인 > 임시 저장 > 리팩토링 > Repository type 강화

--- src/test/reactjs/src/entity/Project.ts
+++ src/test/reactjs/src/entity/Project.ts
@@ -1,8 +1,9 @@
 import {entity} from "src/reactbootdev/decorator/Entity";
 import {SubProject} from "src/entity/SubProject";
+import BaseEntity from "src/reactbootdev/entity/BaseEntity";
 
 @entity
-export class Project {
+export class Project extends BaseEntity {
 
     testcol1?: string
     testcol2?: string
--- src/test/reactjs/src/repository/ProjectRepository.ts
+++ src/test/reactjs/src/repository/ProjectRepository.ts
@@ -1,8 +1,9 @@
 import BaseRepository from "src/reactbootdev/repository/BaseRepository";
 import BaseEntity from "src/reactbootdev/entity/BaseEntity";
 import { v4 as uuidv4 } from 'uuid';
+import {Project} from "src/entity/Project";
 
-export class ProjectRepository extends BaseRepository<BaseEntity> {
+export class ProjectRepository extends BaseRepository<Project> {
     static defaultRepositoryKey = uuidv4()
 
 }
