0914c8c4247d7936278e7c729619f312d30ecfb0
shj0000 <shj0000@ex-it.co.kr>
2023-08-22 00:35:19 +0900

    작동 확인 > 임시 저장 > Bean 출력 및 오류 수정

--- src/test/reactjs/src/reactbootdev/data/EntityBean.ts
+++ src/test/reactjs/src/reactbootdev/data/EntityBean.ts
@@ -3,11 +3,13 @@ import { TaskBeansType,ObjectTypeEnum } from "../interface/TaskBeansType";
 
 import { SubProject } from "src/entity/SubProject";
 import { Project } from "src/entity/Project";
+import { TestEnum } from "src/entity/Project";
 
 
 export const entityImportMap: { [key: string]: any } = {
 	"src/entity/SubProject.ts////SubProject" : SubProject,
-	"src/entity/Project.ts////Project" : Project
+	"src/entity/Project.ts////Project" : Project,
+	"src/entity/Project.ts////TestEnum" : TestEnum,
 }
 
 export const entityBeans: TaskBeansType = {
@@ -66,14 +68,16 @@ export const entityBeans: TaskBeansType = {
             "decorators": [
               {
                 "name": "render",
-                "definition": "(config: { name: string; }) => (target: any, key: string) => void",
+                "definition": "(config: { groupName: string; rendererName: string; propName: string; }) => (target: any, key: string) => void",
                 "arguments": [
                   {
                     "type": "any",
                     "value": {
-                      "name": "testrenderer"
+                      "groupName": "testrenderer",
+                      "rendererName": "testrenderer",
+                      "propName": "testcol1a"
                     },
-                    "txt": "{name: \"testrenderer\"}"
+                    "txt": "{groupName: \"testrenderer\", rendererName: \"testrenderer\", propName: \"testcol1a\"}"
                   }
                 ]
               }
@@ -88,6 +92,16 @@ export const entityBeans: TaskBeansType = {
             "type": "number",
             "isArray": false,
             "isTypeReferenceNode": false
+          },
+          "testcol5a": {
+            "type": "TestEnum",
+            "isArray": false,
+            "isTypeReferenceNode": true
+          },
+          "testcol6a": {
+            "type": "TestEnum",
+            "isArray": true,
+            "isTypeReferenceNode": true
           },
           "subProject": {
             "type": "SubProject",
@@ -124,18 +138,100 @@ export const entityBeans: TaskBeansType = {
         "decorators": [
           {
             "name": "renderContainer",
-            "definition": "(config: { name: string; }) => (target: any) => void",
+            "definition": "(config: { groupName: string; rendererName: string; }) => (target: any) => void",
             "arguments": [
               {
                 "type": "any",
                 "value": {
-                  "name": "testrenderer"
+                  "groupName": "testrenderer",
+                  "rendererName": "testrenderer"
                 },
-                "txt": "{name: \"testrenderer\"}"
+                "txt": "{groupName: \"testrenderer\", rendererName: \"testrenderer\"}"
               }
             ]
           }
         ]
+      },
+      "TestEnum": {
+        "type": ObjectTypeEnum.ENUM,
+        "data": {
+          "test1": "\"test1\"",
+          "test2": "\"test2\"",
+          "test3": "\"test3\""
+        }
+      }
+    }
+  },
+  "src/test/reactjs/node_modules/axios/index.d.ts": {
+    "importPaths": {},
+    "objects": {
+      "HttpStatusCode": {
+        "type": ObjectTypeEnum.ENUM,
+        "data": {
+          "Continue": "100",
+          "SwitchingProtocols": "101",
+          "Processing": "102",
+          "EarlyHints": "103",
+          "Ok": "200",
+          "Created": "201",
+          "Accepted": "202",
+          "NonAuthoritativeInformation": "203",
+          "NoContent": "204",
+          "ResetContent": "205",
+          "PartialContent": "206",
+          "MultiStatus": "207",
+          "AlreadyReported": "208",
+          "ImUsed": "226",
+          "MultipleChoices": "300",
+          "MovedPermanently": "301",
+          "Found": "302",
+          "SeeOther": "303",
+          "NotModified": "304",
+          "UseProxy": "305",
+          "Unused": "306",
+          "TemporaryRedirect": "307",
+          "PermanentRedirect": "308",
+          "BadRequest": "400",
+          "Unauthorized": "401",
+          "PaymentRequired": "402",
+          "Forbidden": "403",
+          "NotFound": "404",
+          "MethodNotAllowed": "405",
+          "NotAcceptable": "406",
+          "ProxyAuthenticationRequired": "407",
+          "RequestTimeout": "408",
+          "Conflict": "409",
+          "Gone": "410",
+          "LengthRequired": "411",
+          "PreconditionFailed": "412",
+          "PayloadTooLarge": "413",
+          "UriTooLong": "414",
+          "UnsupportedMediaType": "415",
+          "RangeNotSatisfiable": "416",
+          "ExpectationFailed": "417",
+          "ImATeapot": "418",
+          "MisdirectedRequest": "421",
+          "UnprocessableEntity": "422",
+          "Locked": "423",
+          "FailedDependency": "424",
+          "TooEarly": "425",
+          "UpgradeRequired": "426",
+          "PreconditionRequired": "428",
+          "TooManyRequests": "429",
+          "RequestHeaderFieldsTooLarge": "431",
+          "UnavailableForLegalReasons": "451",
+          "InternalServerError": "500",
+          "NotImplemented": "501",
+          "BadGateway": "502",
+          "ServiceUnavailable": "503",
+          "GatewayTimeout": "504",
+          "HttpVersionNotSupported": "505",
+          "VariantAlsoNegotiates": "506",
+          "InsufficientStorage": "507",
+          "LoopDetected": "508",
+          "NotExtended": "510",
+          "NetworkAuthenticationRequired": "511"
+        }
       }
     }
   }
--- src/test/reactjs/src/reactbootdev/data/PageBean.ts
+++ src/test/reactjs/src/reactbootdev/data/PageBean.ts
@@ -1,19 +1,30 @@
+
+
+import { CreatePage } from "src/page/TestPage";
+import { ReadListPage } from "src/page/TestPage";
+import { ReadDetailPage } from "src/page/TestPage";
+import { UpdatePage } from "src/page/TestPage";
+import { DeltePage } from "src/page/TestPage";
+import { Welcome } from "src/page/home";
+import { TestEnum } from "src/entity/Project";
 import { TaskBeansType,ObjectTypeEnum } from "../interface/TaskBeansType";
 
 
-import { TestPageA2 } from "src/page/TestPage";
-import { Welcome } from "src/page/home";
-
-
 export const pageImportMap: { [key: string]: any } = {
-	"src/page/TestPage.tsx////TestPageA2" : TestPageA2,
-	"src/page/home.tsx////Welcome" : Welcome
+	"src/page/TestPage.tsx////CreatePage" : CreatePage,
+	"src/page/TestPage.tsx////ReadListPage" : ReadListPage,
+	"src/page/TestPage.tsx////ReadDetailPage" : ReadDetailPage,
+	"src/page/TestPage.tsx////UpdatePage" : UpdatePage,
+	"src/page/TestPage.tsx////DeltePage" : DeltePage,
+	"src/page/home.tsx////Welcome" : Welcome,
+	"src/entity/Project.ts////TestEnum" : TestEnum,
 }
 
 export const pageBeans: TaskBeansType = {
   "src/page/TestPage.tsx": {
     "importPaths": {
       "React": "src/page/react.ts",
+      "useEffect": "src/page/react.ts",
       "useRecoilState": "src/page/recoil.ts",
       "useRecoilValue": "src/page/recoil.ts",
       "BaseRepository": "src/reactbootdev/repository/BaseRepository.ts",
@@ -22,10 +33,11 @@ export const pageBeans: TaskBeansType = {
       "entityRenderer": "src/reactbootdev/component/BaseComponentManager.ts",
       "RenderTypeEnum": "src/reactbootdev/component/BaseComponentManager.ts",
       "ProjectRepository": "src/repository/ProjectRepository.ts",
-      "ProjectApi": "src/api/ProjectApi.ts"
+      "ProjectApi": "src/api/ProjectApi.ts",
+      "TestProjectApi": "src/api/TestProjectApi.ts"
     },
     "objects": {
-      "TestPageA2": {
+      "CreatePage": {
         "type": ObjectTypeEnum.CLASS,
         "data": {},
         "decorators": [
@@ -35,8 +47,76 @@ export const pageBeans: TaskBeansType = {
             "arguments": [
               {
                 "type": "string",
-                "value": "/test",
-                "txt": "\"/test\""
+                "value": "/c",
+                "txt": "\"/c\""
+              }
+            ]
+          }
+        ]
+      },
+      "ReadListPage": {
+        "type": ObjectTypeEnum.CLASS,
+        "data": {},
+        "decorators": [
+          {
+            "name": "page",
+            "definition": "any",
+            "arguments": [
+              {
+                "type": "string",
+                "value": "/r",
+                "txt": "\"/r\""
+              }
+            ]
+          }
+        ]
+      },
+      "ReadDetailPage": {
+        "type": ObjectTypeEnum.CLASS,
+        "data": {},
+        "decorators": [
+          {
+            "name": "page",
+            "definition": "any",
+            "arguments": [
+              {
+                "type": "string",
+                "value": "/rd",
+                "txt": "\"/rd\""
+              }
+            ]
+          }
+        ]
+      },
+      "UpdatePage": {
+        "type": ObjectTypeEnum.CLASS,
+        "data": {},
+        "decorators": [
+          {
+            "name": "page",
+            "definition": "any",
+            "arguments": [
+              {
+                "type": "string",
+                "value": "/u",
+                "txt": "\"/u\""
+              }
+            ]
+          }
+        ]
+      },
+      "DeltePage": {
+        "type": ObjectTypeEnum.CLASS,
+        "data": {},
+        "decorators": [
+          {
+            "name": "page",
+            "definition": "any",
+            "arguments": [
+              {
+                "type": "string",
+                "value": "/d",
+                "txt": "\"/d\""
               }
             ]
           }
@@ -65,6 +145,96 @@ export const pageBeans: TaskBeansType = {
             ]
           }
         ]
+      }
+    }
+  },
+  "src/entity/Project.ts": {
+    "importPaths": {
+      "entity": "src/reactbootdev/decorator/Entity.ts",
+      "SubProject": "src/entity/SubProject.ts",
+      "BaseEntity": "src/reactbootdev/entity/BaseEntity.ts"
+    },
+    "objects": {
+      "TestEnum": {
+        "type": ObjectTypeEnum.ENUM,
+        "data": {
+          "test1": "\"test1\"",
+          "test2": "\"test2\"",
+          "test3": "\"test3\""
+        }
+      }
+    }
+  },
+  "src/test/reactjs/node_modules/axios/index.d.ts": {
+    "importPaths": {},
+    "objects": {
+      "HttpStatusCode": {
+        "type": ObjectTypeEnum.ENUM,
+        "data": {
+          "Continue": "100",
+          "SwitchingProtocols": "101",
+          "Processing": "102",
+          "EarlyHints": "103",
+          "Ok": "200",
+          "Created": "201",
+          "Accepted": "202",
+          "NonAuthoritativeInformation": "203",
+          "NoContent": "204",
+          "ResetContent": "205",
+          "PartialContent": "206",
+          "MultiStatus": "207",
+          "AlreadyReported": "208",
+          "ImUsed": "226",
+          "MultipleChoices": "300",
+          "MovedPermanently": "301",
+          "Found": "302",
+          "SeeOther": "303",
+          "NotModified": "304",
+          "UseProxy": "305",
+          "Unused": "306",
+          "TemporaryRedirect": "307",
+          "PermanentRedirect": "308",
+          "BadRequest": "400",
+          "Unauthorized": "401",
+          "PaymentRequired": "402",
+          "Forbidden": "403",
+          "NotFound": "404",
+          "MethodNotAllowed": "405",
+          "NotAcceptable": "406",
+          "ProxyAuthenticationRequired": "407",
+          "RequestTimeout": "408",
+          "Conflict": "409",
+          "Gone": "410",
+          "LengthRequired": "411",
+          "PreconditionFailed": "412",
+          "PayloadTooLarge": "413",
+          "UriTooLong": "414",
+          "UnsupportedMediaType": "415",
+          "RangeNotSatisfiable": "416",
+          "ExpectationFailed": "417",
+          "ImATeapot": "418",
+          "MisdirectedRequest": "421",
+          "UnprocessableEntity": "422",
+          "Locked": "423",
+          "FailedDependency": "424",
+          "TooEarly": "425",
+          "UpgradeRequired": "426",
+          "PreconditionRequired": "428",
+          "TooManyRequests": "429",
+          "RequestHeaderFieldsTooLarge": "431",
+          "UnavailableForLegalReasons": "451",
+          "InternalServerError": "500",
+          "NotImplemented": "501",
+          "BadGateway": "502",
+          "ServiceUnavailable": "503",
+          "GatewayTimeout": "504",
+          "HttpVersionNotSupported": "505",
+          "VariantAlsoNegotiates": "506",
+          "InsufficientStorage": "507",
+          "LoopDetected": "508",
+          "NotExtended": "510",
+          "NetworkAuthenticationRequired": "511"
+        }
       }
     }
   }
