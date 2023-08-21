e6f399abea3c5369573f3f01efdd7f4d204f34c0
shj0000 <shj0000@ex-it.co.kr>
2023-08-22 00:11:51 +0900

    작동 확인 > 임시 저장 > component, page 분리

--- src/test/reactjs/src/page/TestPage.tsx
+++ src/test/reactjs/src/page/TestPage.tsx
@@ -10,33 +10,15 @@ import {TestProjectApi} from "src/api/TestProjectApi";
 
 
 
-const BasePageContent = () => {
-    const projectApi = new TestProjectApi()
+const ReadListComponent = () => {
 
-    // create
-    const createProjectRepository = new ProjectRepository(ProjectRepository.defaultRepositoryKey + `create`)
-    const [createEntityList, setCreateEntityList] = useRecoilState(createProjectRepository.entityListState);
-    createProjectRepository.init(createEntityList, setCreateEntityList);
+    // api
+    const projectApi = new TestProjectApi()
 
     // readList
     const readListProjectRepository = new ProjectRepository(ProjectRepository.defaultRepositoryKey + `readList`)
     const [readListEntityList, setReadListEntityList] = useRecoilState(readListProjectRepository.entityListState);
     readListProjectRepository.init(readListEntityList, setReadListEntityList);
-
-    // readDetail
-    const readDetailProjectRepository = new ProjectRepository(ProjectRepository.defaultRepositoryKey + `readDetail`)
-    const [readDetailEntityList, setReadDetailEntityList] = useRecoilState(readDetailProjectRepository.entityListState);
-    readDetailProjectRepository.init(readDetailEntityList, setReadDetailEntityList);
-
-    // update
-    const updateProjectRepository = new ProjectRepository(ProjectRepository.defaultRepositoryKey + `update`)
-    const [updateEntityList, setUpdateEntityList] = useRecoilState(updateProjectRepository.entityListState);
-    updateProjectRepository.init(updateEntityList, setUpdateEntityList);
-
-    // delete
-    const deleteProjectRepository = new ProjectRepository(ProjectRepository.defaultRepositoryKey + `delete`)
-    const [deleteEntityList, setDeleteEntityList] = useRecoilState(deleteProjectRepository.entityListState);
-    deleteProjectRepository.init(deleteEntityList, setDeleteEntityList);
 
     // set readDetailProjectRepository by projectApi
     useEffect(() => {
@@ -45,30 +27,7 @@ const BasePageContent = () => {
         const resData = readListRes.result.data as Project[]
         readListProjectRepository.addEntities(resData)
 
-        // readDetail
-        const readDetailRes = projectApi.handleReadDetail(undefined)
-        const readDetailResData = readDetailRes.result.data as Project
-        readDetailProjectRepository.addEntity(readDetailResData)
-
-        // update
-        const updateRes = projectApi.handleReadDetail(undefined)
-        const updateResData = updateRes.result.data as Project
-        updateProjectRepository.addEntity(updateResData)
-
     }, [])
-
-
-    // TODO :: api에 의한 repo update는 두 곳에서. `page`랑 `component`
-
-    const createEntity = entityRenderer(
-        Project,
-        createProjectRepository,
-        projectApi,
-        RenderTypeEnum.CREATE,
-        {
-            itemId: 0,
-        },
-    )
 
     const readListEntity = entityRenderer(
         Project,
@@ -76,6 +35,43 @@ const BasePageContent = () => {
         projectApi,
         RenderTypeEnum.READ_LIST
     )
+
+
+    return (
+        <div>
+            <div>
+                defaultRepositoryKey : {BaseRepository.defaultRepositoryKey}
+            </div>
+
+            <div>--- --- ---</div>
+            <div>{readListEntity}</div>
+            <div>--- --- ---</div>
+
+
+            <div>{JSON.stringify(readListEntityList)}</div>
+            {/*<button onClick={addDate}>aadd</button>*/}
+        </div>
+    );
+
+}
+
+const ReadDetailComponent = () => {
+
+    // api
+    const projectApi = new TestProjectApi()
+
+    // readDetail
+    const readDetailProjectRepository = new ProjectRepository(ProjectRepository.defaultRepositoryKey + `readDetail`)
+    const [readDetailEntityList, setReadDetailEntityList] = useRecoilState(readDetailProjectRepository.entityListState);
+    readDetailProjectRepository.init(readDetailEntityList, setReadDetailEntityList);
+
+    // set readDetailProjectRepository by projectApi
+    useEffect(() => {
+        // readDetail
+        const readDetailRes = projectApi.handleReadDetail(undefined)
+        const readDetailResData = readDetailRes.result.data as Project
+        readDetailProjectRepository.addEntity(readDetailResData)
+    }, [])
 
     const readDetailEntity = entityRenderer(
         Project,
@@ -87,6 +83,41 @@ const BasePageContent = () => {
         },
     )
 
+    return (
+        <div>
+            <div>
+                defaultRepositoryKey : {BaseRepository.defaultRepositoryKey}
+            </div>
+
+            <div>--- --- ---</div>
+            <div>{readDetailEntity}</div>
+            <div>--- --- ---</div>
+
+
+            <div>{JSON.stringify(readDetailEntityList)}</div>
+        </div>
+    );
+
+}
+
+const UpdateComponent = () => {
+
+    // api
+    const projectApi = new TestProjectApi()
+
+    // update
+    const updateProjectRepository = new ProjectRepository(ProjectRepository.defaultRepositoryKey + `update`)
+    const [updateEntityList, setUpdateEntityList] = useRecoilState(updateProjectRepository.entityListState);
+    updateProjectRepository.init(updateEntityList, setUpdateEntityList);
+
+    // set readDetailProjectRepository by projectApi
+    useEffect(() => {
+        // update
+        const updateRes = projectApi.handleReadDetail(undefined)
+        const updateResData = updateRes.result.data as Project
+        updateProjectRepository.addEntity(updateResData)
+    }, [])
+
     const updateEntity = entityRenderer(
         Project,
         updateProjectRepository,
@@ -96,6 +127,33 @@ const BasePageContent = () => {
             itemId: 0,
         },
     )
+
+    return (
+        <div>
+            <div>
+                defaultRepositoryKey : {BaseRepository.defaultRepositoryKey}
+            </div>
+
+            <div>--- --- ---</div>
+            <div>{updateEntity}</div>
+            <div>--- --- ---</div>
+
+            <div>{JSON.stringify(updateEntityList)}</div>
+            {/*<button onClick={addDate}>aadd</button>*/}
+        </div>
+    );
+
+}
+
+
+const DeleteComponent = () => {
+    // api
+    const projectApi = new TestProjectApi()
+
+    // delete
+    const deleteProjectRepository = new ProjectRepository(ProjectRepository.defaultRepositoryKey + `delete`)
+    const [deleteEntityList, setDeleteEntityList] = useRecoilState(deleteProjectRepository.entityListState);
+    deleteProjectRepository.init(deleteEntityList, setDeleteEntityList);
 
     const deleteEntity = entityRenderer(
         Project,
@@ -107,31 +165,41 @@ const BasePageContent = () => {
         },
     )
 
-    // TODO :: 아래와 같은 형태로 변환예정. 변수는 상속관계.
-    // TODO :: 일단은 동작 가능한 최소의 프로토타입 코드 작성. CRRUD.
-    // TODO :: key 가 값으면 같은 recoil 참고됨. 어떻게 파일별로 중복 방지해서 관리할 수 있을지 생각해봐. 데코레이터 활용?? > 엔티티에 키값 설정 디폴트는 클레스 네임?
+    return (
+        <div>
+            <div>
+                defaultRepositoryKey : {BaseRepository.defaultRepositoryKey}
+            </div>
+
+            <div>--- --- ---</div>
+            <div>{deleteEntity}</div>
+            <div>--- --- ---</div>
 
 
-    // TODO :: entity 합성 관계는 재귀적으로 호출하고, 순환참조 에러 띄우고, 기초타입까지 전수조사로 출력하면 되는건가?
-    // TODO :: 반드시 순환참조가 필요하다면..?
+            <div>{JSON.stringify(deleteEntityList)}</div>
+            {/*<button onClick={addDate}>aadd</button>*/}
+        </div>
+    );
+}
 
-    // TODO :: 객체 타입을 포함한 재귀형태 entity 를 json 형태로 변환하는 것. 이것이 가장 큰 기술적 난제야.
+const CreateComponent = () => {
+    // api
+    const projectApi = new TestProjectApi()
 
+    // create
+    const createProjectRepository = new ProjectRepository(ProjectRepository.defaultRepositoryKey + `create`)
+    const [createEntityList, setCreateEntityList] = useRecoilState(createProjectRepository.entityListState);
+    createProjectRepository.init(createEntityList, setCreateEntityList);
 
-//     const addDate = () => {
-//         projectRepository.addEntities(
-// [{
-//             id: 314
-//         },
-//         {
-//             id: 3142
-//         }])
-//     }
-
-    // TODO :: API Generator.
-    // TODO :: renderedEntity > Container
-    // TODO :: BaseDomain > Pagenation, Search
-
+    const createEntity = entityRenderer(
+        Project,
+        createProjectRepository,
+        projectApi,
+        RenderTypeEnum.CREATE,
+        {
+            itemId: 0,
+        },
+    )
 
     return (
         <div>
@@ -143,40 +211,43 @@ const BasePageContent = () => {
             <div>{createEntity}</div>
             <div>--- --- ---</div>
 
-            <div>--- --- ---</div>
-            <div>{readDetailEntity}</div>
-            <div>--- --- ---</div>
 
-            <div>--- --- ---</div>
-            <div>{readListEntity}</div>
-            <div>--- --- ---</div>
-
-            <div>--- --- ---</div>
-            <div>{updateEntity}</div>
-            <div>--- --- ---</div>
-
-            <div>--- --- ---</div>
-            <div>{deleteEntity}</div>
-            <div>--- --- ---</div>
-
-
-            <div>{JSON.stringify(readListEntityList)}</div>
+            <div>{JSON.stringify(createEntityList)}</div>
             {/*<button onClick={addDate}>aadd</button>*/}
         </div>
     );
+
 };
 
 
-@page("/test")
-export class TestPageA2 {
-
-
-    // @Autowired
-    // baseRepository: BaseRepository;
-
-
+@page("/c")
+export class CreatePage {
     render() {
-        return <BasePageContent />;
+        return <CreateComponent />;
     }
-
 }
+@page("/r")
+export class ReadListPage {
+    render() {
+        return <ReadListComponent />;
+    }
+}
+@page("/rd")
+export class ReadDetailPage {
+    render() {
+        return <ReadDetailComponent />;
+    }
+}
+@page("/u")
+export class UpdatePage {
+    render() {
+        return <UpdateComponent />;
+    }
+}
+@page("/d")
+export class DeltePage {
+    render() {
+        return <DeleteComponent />;
+    }
+}
+
