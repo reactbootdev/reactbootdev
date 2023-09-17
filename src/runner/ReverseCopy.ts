b3dc1e63d9e9c592d25a7c0166ce25ced242068b
shj0000 <shj0000@ex-it.co.kr>
2023-09-17 20:41:24 +0900

    작동 확인 > npm run script > `reverseCopy`

---
+++ src/runner/ReverseCopy.ts
@@ -0,0 +1,42 @@
+const fs = require('fs');
+const path = require('path');
+
+
+function copyFilesRecursively(sourceDir: string, targetDir: string) {
+    // sourceDir에 있는 모든 파일/폴더를 읽어옵니다.
+    const items = fs.readdirSync(sourceDir);
+
+    for (const item of items) {
+        const sourcePath = path.join(sourceDir, item);
+        const targetPath = path.join(targetDir, item);
+
+        const stat = fs.lstatSync(sourcePath);
+
+        if (stat.isFile()) {
+            // 파일인 경우
+            if (!sourcePath.includes('interface')) {
+                // interface 폴더 내 파일이 아닌 경우만 처리
+                const parsedPath = path.parse(targetPath);
+                const newPath = path.format({
+                    ...parsedPath,
+                    base: parsedPath.name  + parsedPath.ext + '.txt',
+                });
+
+                fs.copyFileSync(sourcePath, newPath);
+                console.log(`파일 복사 및 확장자 추가: ${sourcePath} -> ${newPath}`);
+            } else {
+                // interface 폴더 내 파일인 경우 그냥 복사
+                fs.copyFileSync(sourcePath, targetPath);
+                console.log(`파일 복사: ${sourcePath} -> ${targetPath}`);
+            }
+        } else if (stat.isDirectory()) {
+            // 폴더인 경우 재귀적으로 복사합니다.
+            fs.mkdirSync(targetPath, { recursive: true }); // 폴더 생성
+            copyFilesRecursively(sourcePath, targetPath); // 재귀 호출
+        }
+    }
+}
+
+const srcPath = path.join(__dirname, '..', 'src', 'test', 'reactjs', 'src', 'reactbootdev');
+const destPath = path.join(__dirname, '..', 'src', 'runner', 'copy');
+copyFilesRecursively(srcPath, destPath)
