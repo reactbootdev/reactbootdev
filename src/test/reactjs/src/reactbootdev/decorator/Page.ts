4846314f0a7e48a6947697b6770f92c9bdb3788c
shj0000 <shj0000.ex.it@gmail.com>
2023-10-05 01:37:06 +0900

    Edit `@page` log

--- src/test/reactjs/src/reactbootdev/decorator/Page.ts
+++ src/test/reactjs/src/reactbootdev/decorator/Page.ts
@@ -1,6 +1,5 @@
 export function page(pageUrl: string) {
     return function (target: any) {
-        console.debug(`URL | Class Name > '${pageUrl}' | '${target.name}'`);
         return target;
     };
 }
