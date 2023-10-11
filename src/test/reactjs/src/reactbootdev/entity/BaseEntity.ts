f9217760245a77c322ef1a8db57e8527d33ce267
shj0000 <shj0000.ex.it@gmail.com>
2023-10-12 00:59:01 +0900

    edit BaseEntity.ts for extractProperties

--- src/test/reactjs/src/reactbootdev/entity/BaseEntity.ts
+++ src/test/reactjs/src/reactbootdev/entity/BaseEntity.ts
@@ -1,5 +1,10 @@
+import {extractProperties} from "@src/reactbootdev/decorator/Property";
+
 export default class BaseEntity {
 
     id: number | null = null;
 
+    static extractProperties(): any {
+        return extractProperties(this);
+    }
 }
